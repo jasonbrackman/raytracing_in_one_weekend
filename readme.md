a644072a6406758dd33fdab377273dfe6c9e0dc6
jasonbrackman <brackman@gmail.com>
2018-11-01 21:51:44 -0700

    adding Ray Struct and a camera

--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ Attempting to follow https://github.com/petershirley/raytracinginoneweekend
 Step 1: Create a PPM file
 
 <img src="ppm_example.png" width="480" height="420" />
+
+Step 2: Render a "Linear Blend" or "Linear Interpolation"
+
+<img src="ppm_lerp.png" width="480" height="420" />
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,37 @@ use std::io::{Write, Result};
 use std::fs::File;
 use std::ops::{Add, Sub, Mul, Div};
 
-
+#[derive(Copy, Clone)]
 struct Vec3 {
     e: [f64; 3],
 }
 
 impl Vec3 {
-    fn x(self) -> f64 {self.e[0]}
-    fn y(self) -> f64 {self.e[1]}
-    fn z(self) -> f64 {self.e[2]}
+    pub fn x(self) -> f64 {self.e[0]}
+    pub fn y(self) -> f64 {self.e[1]}
+    pub fn z(self) -> f64 {self.e[2]}
 
-    fn r(self) -> f64 {self.e[0]}
-    fn g(self) -> f64 {self.e[1]}
-    fn b(self) -> f64 {self.e[2]}
+    pub fn r(self) -> f64 {self.e[0]}
+    pub fn g(self) -> f64 {self.e[1]}
+    pub fn b(self) -> f64 {self.e[2]}
 
 }
 
 impl Vec3 {
-    fn new(v:[f64; 3]) -> Self {
+    pub fn new(v:[f64; 3]) -> Self {
         Vec3{e: v}
     }
 
-    fn length(self) -> f64{
+    pub fn length(&self) -> f64 {
         (self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]).sqrt()
+    }
+
+    pub fn mul_by_float(self, t: f64) -> Vec3 {
+        Vec3{e:[t * self.e[0], t * self.e[1], t * self.e[2]]}
+    }
+
+    pub fn div_by_float(self, t: f64) -> Vec3 {
+        Vec3{e:[ self.e[0]/t, self.e[1] / t, self.e[2] / t]}
     }
 }
 
@@ -78,18 +86,19 @@ struct Ray {
 impl Ray {
     fn new(a_: Vec3, b_: Vec3) -> Self { Ray{a: a_, b: b_} }
 
-    fn origin(self) -> Vec3 {
+    pub fn origin(self) -> Vec3 {
         self.a
     }
 
-    fn direction(self) -> Vec3 {
+    pub fn direction(self) -> Vec3 {
         self.b
     }
 
-    fn point_of_parameter(self, t: f64) -> f64 {
-        self.a + t * self.b
+    pub fn point_of_parameter(self, t: f64) -> Vec3 {
+        self.a + self.b.mul_by_float(t)
     }
 }
+
 fn ppm_example() -> Result<()> {
 
     let mut buffer = File::create("helloworld.ppm")?;
@@ -101,9 +110,23 @@ fn ppm_example() -> Result<()> {
 
     write!(buffer, "P3\n{} {}\n255\n", nx, ny);
 
+    let lower_left_corner = Vec3{e:[-2.0, -1.0, -1.0]};
+    let horizontal = Vec3{e:[4.0, 0.0, 0.0]};
+    let vertical = Vec3{e:[0.0, 2.0, 0.0]};
+    let origin = Vec3{e:[0.0, 0.0, 0.0]};
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new([i as f64 / nx as f64, j as f64 / ny as f64, 0.2]);
+            let u = i as f64 / nx as f64;
+            let v = j as f64 / ny as f64;
+
+            let r = Ray::new(origin,  lower_left_corner +
+                                                    horizontal.mul_by_float(u) +
+                                                    vertical.mul_by_float(v));
+
+            let col = color(r);
+            // let col = Vec3::new([i as f64 / nx as f64, j as f64 / ny as f64, 0.2]);
+
             let ir = (255.99 * col.e[0]) as i64;
             let ig = (255.99 * col.e[1]) as i64;
             let ib = (255.99 * col.e[2]) as i64;
@@ -114,6 +137,17 @@ fn ppm_example() -> Result<()> {
     Ok(())
 }
 
+fn unit_vector(v: Vec3) -> Vec3 {
+    let length = v.length();
+    v.div_by_float(length)
+}
+
+fn color(r: Ray) -> Vec3 {
+    let unit_direction = unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    Vec3{e:[1.0, 1.0, 1.0]}.mul_by_float(1.0 - t) + Vec3{e:[0.5, 0.7, 1.0]}.mul_by_float(t)
+}
+
 fn main() {
-    ppm_example();
+    let _x = ppm_example().unwrap();
 }
