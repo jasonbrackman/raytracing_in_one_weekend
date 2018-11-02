9c26a5af0231701371b1ba917f84ad317c0f2ead
jason_w <brackman@gmail.com>
2018-11-01 19:19:38 -0700

    adding a ray struct, and filling out the vec3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{Write, Result};
 use std::fs::File;
-use std::ops::{Add, Mul};
+use std::ops::{Add, Sub, Mul, Div};
 
 
 struct Vec3 {
@@ -22,10 +22,14 @@ impl Vec3 {
     fn new(v:[f64; 3]) -> Self {
         Vec3{e: v}
     }
+
+    fn length(self) -> f64{
+        (self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]).sqrt()
+    }
 }
 
 impl Add for Vec3 {
-    type Output = Self;
+    type Output = Vec3;
 
     fn add(self, rhs:Vec3) -> Self {
         Vec3{e: [self.e[0] + rhs.e[0],
@@ -34,7 +38,58 @@ impl Add for Vec3 {
     }
 }
 
+impl Sub for Vec3 {
+    type Output = Vec3;
 
+    fn sub(self, rhs:Vec3) -> Self {
+        Vec3{e: [self.e[0] - rhs.e[0],
+                 self.e[1] - rhs.e[1],
+                 self.e[2] - rhs.e[2]]}
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs:Vec3) -> Self {
+        Vec3{e: [self.e[0] * rhs.e[0],
+                 self.e[1] * rhs.e[1],
+                 self.e[2] * rhs.e[2]]}
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs:Vec3) -> Self {
+        Vec3{e: [self.e[0] / rhs.e[0],
+                 self.e[1] / rhs.e[1],
+                 self.e[2] / rhs.e[2]]}
+    }
+}
+
+
+
+struct Ray {
+    a: Vec3,
+    b: Vec3
+}
+
+impl Ray {
+    fn new(a_: Vec3, b_: Vec3) -> Self { Ray{a: a_, b: b_} }
+
+    fn origin(self) -> Vec3 {
+        self.a
+    }
+
+    fn direction(self) -> Vec3 {
+        self.b
+    }
+
+    fn point_of_parameter(self, t: f64) -> f64 {
+        self.a + t * self.b
+    }
+}
 fn ppm_example() -> Result<()> {
 
     let mut buffer = File::create("helloworld.ppm")?;
@@ -48,9 +103,10 @@ fn ppm_example() -> Result<()> {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let ir = (255.99 * (i as f64 / nx as f64)) as i64;
-            let ig = (255.99 * (j as f64 / ny as f64)) as i64;
-            let ib = (255.99 * 0.2) as i64;
+            let col = Vec3::new([i as f64 / nx as f64, j as f64 / ny as f64, 0.2]);
+            let ir = (255.99 * col.e[0]) as i64;
+            let ig = (255.99 * col.e[1]) as i64;
+            let ib = (255.99 * col.e[2]) as i64;
 
             write!(buffer, "{} {} {}\n", ir, ig, ib);
         }
